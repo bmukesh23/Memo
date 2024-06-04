d802d74bdc7a55de032dcd1308365e094d0312c2
Mukesh <billamukesh1234@gmail.com>
2024-06-05 00:52:44 +0530

    add: tag-input feature in modal

--- frontend/src/pages/Home/AddEditNotes.jsx
+++ frontend/src/pages/Home/AddEditNotes.jsx
@@ -1,12 +1,55 @@
-const AddEditNotes = () => {
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import TagInput from "../../components/Input/TagInput"
+import { MdClose } from "react-icons/md";
+
+const AddEditNotes = ({noteData, type, onClose}) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const [error, setError] = useState(null);
+
+  // Add Note
+  const addNewNote = async () => {}
+
+  // Edit Note
+  const editNote = async () => {}
+
+  const handleAddNote = () => {
+    if(!title){
+      setError("Please enter the title");
+      return;
+    }
+
+    if(!content){
+      setError("Please enter the content");
+      return;
+    }
+
+    setError("");
+
+    if(type === "edit"){
+      editNote();
+    } else{
+      addNewNote();
+    }
+  }
+
   return (
-    <div>
+    <div className="relative">
+      <button className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50" onClick={onClose}>
+          <MdClose className="text-xl text-slate-400"/>
+      </button>
+
       <div className="flex flex-col gap-2">
         <label className="input-label">TITLE</label>
         <input
           type="text"
           className="text-2xl text-slate-950 outline-none"
           placeholder="Go to Gym at 5pm"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
 
@@ -17,14 +60,19 @@ const AddEditNotes = () => {
           className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
           placeholder="content"
           rows={10}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
       </div>
 
       <div className="mt-3">
         <label className="input-label">TAGS</label>
+        <TagInput tags={tags} setTags={setTags} />
       </div>
 
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+      {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+      <button className="btn-primary font-medium mt-5 p-3" onClick={handleAddNote}>
         ADD
       </button>
     </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
         onClick={() => {
-          setOpenAddEditModal({isShown: true, type: "add", data: null});
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
         }}>
         <MdAdd className="text-[32px] text-white" />
       </button>
@@ -48,9 +48,15 @@ const Home = () => {
           },
         }}
         contentLabel=""
-        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5"
       >
-        <AddEditNotes />
+        <AddEditNotes
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
       </Modal>
     </>
   )
