5aacb2bbdf52a82ccd01bb4b3273290125847399
Mukesh <billamukesh1234@gmail.com>
2024-06-05 20:52:08 +0530

    add: config.json

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
 const app = express();
 
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: "*",
-    })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({data: "hello"});
 });
 
-app.listen(8000, () => console.log("Server Started"));
+app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
 
 module.exports = app;
