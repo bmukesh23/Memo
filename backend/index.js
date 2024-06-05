8047d40f7f0019cab2eb58e39b6061dc28d358e6
Mukesh <billamukesh1234@gmail.com>
2024-06-05 22:59:43 +0530

    add: create account api

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,67 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
-const PORT = process.env.PORT || 8000;
+const User = require("./models/user.model");
+
+const config = {
+    connectionString: process.env.MONGODB_URI,
+    PORT: process.env.PORT || 8000,
+}
+
+mongoose.connect(config.connectionString).then(() => console.log("MongoDB connected")).catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({data: "hello"});
+    res.json({ data: "hello" });
 });
 
-app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    if (!fullName) {
+        return res.status(400).json({ eror: true, message: "Full Name is required" });
+    }
+
+    if (!email) {
+        res.status(400).json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        res.status(400).json({ error: true, message: "Password is required" });
+    }
+
+    const isUser = await User.findOne({ email: email });
+
+    if (isUser) {
+        return res.json({ error: true, message: "User already exists" });
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "360000m" });
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Successful",
+    });
+});
+
+app.listen(config.PORT, () => console.log(`Server Started at ${config.PORT}`));
 
 module.exports = app;
