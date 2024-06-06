1ee502e000d23b9043083d6b70c4c149dded40b3
Mukesh <billamukesh1234@gmail.com>
2024-06-06 11:24:35 +0530

    add: login api

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.post("/create-account", async (req, res) => {
 
     await user.save();
 
-    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "360000m" });
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "36000m" });
 
     return res.json({
         error: false,
@@ -61,6 +61,38 @@ app.post("/create-account", async (req, res) => {
         message: "Registration Successful",
     });
 });
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Password is required" });
+    }
+
+    const userInfo = await User.findOne({ email: email });
+
+    if (!userInfo) {
+        return res.status(400).json({ error: true, message: "User not found" });
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "36000m" });
+
+        return res.json({
+            error: false,
+            message: "Login Successful",
+            accessToken,
+            email,
+        });
+    } else {
+        return res.status(400).json({ error: true, message: "Invalid Credentials" });
+    }
+})
 
 app.listen(config.PORT, () => console.log(`Server Started at ${config.PORT}`));
 
