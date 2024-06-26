f0870d16844aab3b2ec3c2e268b6e55301ff63c6
Mukesh <billamukesh1234@gmail.com>
2024-06-26 23:12:57 +0530

    refactor: backend

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
