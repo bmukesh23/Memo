a2a85b7d4aacb159bd7b040aa48fdb2dd67b519c
Mukesh <billamukesh1234@gmail.com>
2024-06-27 11:57:58 +0530

    update: backend folder

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
