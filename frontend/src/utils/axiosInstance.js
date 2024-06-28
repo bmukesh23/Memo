23c88c6fa00c7b9c60367fa6a75547848492da20
Mukesh <billamukesh1234@gmail.com>
2024-06-29 00:23:46 +0530

    fix: vercel issue

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: "https://memo-ai-backend.vercel.app",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
