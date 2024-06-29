3d73d6e89f75d1075dc854c55cc4abd18a38be5c
Mukesh <billamukesh1234@gmail.com>
2024-06-29 21:47:53 +0530

    update: axios instance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    // baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
