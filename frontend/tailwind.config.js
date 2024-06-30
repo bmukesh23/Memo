ed61721ed426d00f8cd8e8909edd7651d20aed7a
Mukesh <billamukesh1234@gmail.com>
2024-07-01 00:09:21 +0530

    add: shadcn-ui configurations

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        //colors used in the project
-        primary: "#2B85FF",
-        secondary: "#EF863E",
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      }
+
+export const darkMode = ["class"];
+export const content = [
+  './pages/**/*.{js,jsx}',
+  './components/**/*.{js,jsx}',
+  './app/**/*.{js,jsx}',
+  './src/**/*.{js,jsx}',
+];
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
     },
   },
-  plugins: [],
-}
-
+  extend: { 
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+  },
+};
+export const plugins = [import("tailwindcss-animate")];
